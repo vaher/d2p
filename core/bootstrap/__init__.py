9149c90d936c0885e1178f3c400803934aba6fe8
Valerij Herber <valerij@valerij-T520.(none)>
2012-09-04 15:51:58 +0200

    Changes to add multicast bootstrap for d2p

--- core/bootstrap/__init__.py
+++ core/bootstrap/__init__.py
@@ -1,4 +1,6 @@
-
+from .multicastbootstrap import ip_mcast_receiver
+from .multicastbootstrap import membermanager
+from .multicastbootstrap import ip_mcast_sender
 import collections
 
 BootstrapEntry = collections.namedtuple('BootstrapEntry',
@@ -13,7 +15,41 @@ def _registerBootstrap(bootstrapClass):
     return bootstrapClass
 
 def create(data):
+    #print(data)
     return _bootstrap_types[data['bsType']]()
+
+
+@_registerBootstrap
+class MulticastBootstrap(object):
+    bootstrap_type = 'local_multicast'
+    ui_bootstrap_name = 'Multicast bootstrap'
+    
+    def start(self, assignedId, io_loop, getAdvertised, onFind):
+        """ Start running on the specified io_loop """
+        self.assignedId = assignedId
+        self._getAdvertised = getAdvertised
+        self._onFind = onFind
+        self._entries = []
+                
+        memberManager = membermanager.MemberManager(self._getAdvertised, self._onFind)
+        ip_mcast_receiver.MulticastBootstrap(memberManager, io_loop)        
+
+    def renotify(self):
+        """ Call onFind for all entries this bootstrap has found """
+        for e in self._entries:
+            self._onFind(e)
+
+    @property
+    def ui_entries(self):
+        return self._entries
+
+    def ui_addEntry(self, bse):
+        assert isinstance(bse, BootstrapEntry)
+        assert bse.addr
+        self._entries.append(bse)
+        self._onFind(bse)
+
+
 
 @_registerBootstrap
 class ManualBootstrap(object):
